74991655f2ec159a8dd020b869c29ade4f46e3c5
PabloCastilloSegura <pcs00018@red.ujaen.es>
2016-09-29 11:08:04 +0200

    Comentarios para clase

--- practica1/app/src/main/java/es/ujaen/git/practica1/Autentication.java
+++ practica1/app/src/main/java/es/ujaen/git/practica1/Autentication.java
@@ -4,15 +4,19 @@ package es.ujaen.git.practica1;
  * Created by Antonio on 29/09/2016.
  * Nota: Poner las variables en ingles .Para java doc pongo dos asteriscos.
  */
+ /**Clase para recoger los datos*/
 public class Autentication {
 
     public static final int SERVICE_PORT=6000;
-
+	/**Al declararse protected no se pueden usar fuera de la clase, hay que crear un constructor para introducir datos.
+	*/
     protected String mUser="user"; //inicializarlo, orotected es que solo puedo usarlo en clases que heredan
     protected String mPass ="";
     protected String mIP = "127.0.0.1";
     protected int mPort = SERVICE_PORT;
-
+	
+	/**Constructor autenticación
+	*/
     public Autentication(String User, String Pass, int Port, String IP) {
         this.mUser = User;
         this.mPass = Pass;
