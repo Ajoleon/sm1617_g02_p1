4d7cb2df638a66765a5554cb6f85c72c93884345
Antonio <ajls0006@red.ujaen.es>
2016-09-29 12:22:37 +0200

    Sesion 1 completada

--- practica1/app/src/main/java/es/ujaen/git/practica1/MainActivity.java
+++ practica1/app/src/main/java/es/ujaen/git/practica1/MainActivity.java
@@ -1,5 +1,7 @@
 package es.ujaen.git.practica1;
 
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -8,7 +10,15 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.fragment_auth);
+        setContentView(R.layout.activity_main);
+
+        FragmentManager fs = getSupportFragmentManager();
+        FragmentTransaction ft = fs.beginTransaction();
+        AuthFragment au = AuthFragment.newInstance("pepe","123456");//todo esto para ir añadiendo de forma dinamica el fragment
+        ft.add(R.id.main_frame,au);
+       // ft.disallowAddToBackStack(null);//añade a la pila y se puede volver al fragmento anterior, si hay pila
+        ft.commit();
+
     }
 
 
