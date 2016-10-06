caa627f4dbfc7748d13ff9353cc6c5d7973b94e0
Antonio <ajls0006@red.ujaen.es>
2016-10-06 11:10:49 +0200

    Explicacion Sesion 2

--- practica1/app/src/main/java/es/ujaen/git/practica1/MainActivity.java
+++ practica1/app/src/main/java/es/ujaen/git/practica1/MainActivity.java
@@ -4,6 +4,9 @@ import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,14 +17,39 @@ public class MainActivity extends AppCompatActivity {
 
         FragmentManager fs = getSupportFragmentManager();
         FragmentTransaction ft = fs.beginTransaction();
-        AuthFragment au = AuthFragment.newInstance("pepe","123456");//todo esto para ir añadiendo de forma dinamica el fragment
-        ft.add(R.id.main_frame,au);
-       // ft.disallowAddToBackStack(null);//añade a la pila y se puede volver al fragmento anterior, si hay pila
+        AuthFragment au = AuthFragment.newInstance("pepe", "123456");
+        //todo esto para ir añadiendo de forma dinamica el fragment
+        ft.add(R.id.main_frame, au);
+        // ft.disallowAddToBackStack(null);//añade a la pila y se puede volver al fragmento anterior, si hay pila
         ft.commit();
 
+        Button boton = (Button) findViewById(R.id.auth_button_send);
+        boton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View w) {
+                Toast.makeText(MainActivity.this, "Pulsado el 1", Toast.LENGTH_SHORT).show();
+
+            }
+        });
     }
 
+        /**
+         * Este metodo sirve para mostrar una tostada en funcion del boton que pulses
+         *
+         * @param view la vista que ha generado el evento
+         */
 
-
+    public void onSend(View view) {
+        switch (view.getId()) {
+            case R.id.auth_button_send:
+                Toast.makeText(MainActivity.this, "Pulsado el 1", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.auth_button_send2:
+                Toast.makeText(MainActivity.this, "Pulsado el 2", Toast.LENGTH_SHORT).show();
+                break;
+        }
+    }
 
 }
+
+
